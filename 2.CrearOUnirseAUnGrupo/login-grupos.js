e7dbc0e2618d686e64c6871ed7af0b5e9eff33da
Martina Juri <martina.juri@mi.unc.edu.ar>
2022-10-31 21:00:21 -0300

    unificacion grupos
    las paginas 2, 3.1 y 3.2 se reescribieron en la 2 (HTML y CSS), se creó login-grupos.js para manejar las vistas en las diferentes instancias

---
+++ 2.CrearOUnirseAUnGrupo/login-grupos.js
@@ -0,0 +1,9 @@
+const pageGrupos =  document.getElementById("buttons-grupos")
+const pageNuevoGrupo =  document.getElementById("page-nuevo-grupo")
+const pageUnirseGrupo =  document.getElementById("page-unirse-grupo")
+
+console.log(pageGrupos)
+console.log(pageNuevoGrupo)
+
+document.getElementById("pageNuevoGrupo").style.display = "none";
+document.getElementById("pageUnirseGrupo").style.display = "none";
