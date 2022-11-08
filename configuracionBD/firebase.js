65ddc7a5015d50011eea831c44d083543bc3d6f5
Jeftbook <jeftbook@gmail.com>
2022-11-07 21:09:43 -0300

    Cambios en el inicio de sesio, cuidado, puede tener errores

--- configuracionBD/firebase.js
+++ configuracionBD/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app)
 
 console.log(app);
