16b4cdb3a3250559a00db7c3b504061ac7d243db
AgustinAlvarez2001 <agus.alvarez2001@gmail.com>
2022-11-14 13:57:57 -0300

    borro

--- configuracionBD/datosLogin.js
+++
@@ -1,9 +0,0 @@
-function returnDatosLogin(){
-    var nombre = document.getElementById("nombreUsuario")
-    var telefono = document.getElementById("telefonoUsuario")
-    var direccion = document.getElementById("direccionUsuario")
-    if(telefono != ""){
-        return telefono;
-    }
-    alert("anda");///////////
-}
