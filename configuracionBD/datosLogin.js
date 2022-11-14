47c921565c822388456b87b1ed530e6f3cad59c4
AgustinAlvarez2001 <agus.alvarez2001@gmail.com>
2022-11-14 13:54:46 -0300

    correccion Id's

---
+++ configuracionBD/datosLogin.js
@@ -0,0 +1,9 @@
+function returnDatosLogin(){
+    var nombre = document.getElementById("nombreUsuario")
+    var telefono = document.getElementById("telefonoUsuario")
+    var direccion = document.getElementById("direccionUsuario")
+    if(telefono != ""){
+        return telefono;
+    }
+    alert("anda");///////////
+}
